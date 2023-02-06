1f0ae955cff28d091c1f4bc2c4ba474d5d379f0f
vishal9643 <amitkrmishra864@gmail.com>
2023-02-07 02:50:28 +0530

    first-commit

---
+++ src/components/Services.js
@@ -0,0 +1,29 @@
+import Title from "./Title";
+import { Benefits } from "./data";
+
+const Services = () => {
+  return (
+    <div>
+      <section className="section services" id="services">
+        <Title title="our" subtitle="services" />
+        <div className="section-center services-center">
+          {Benefits.map((service) => {
+            return (
+              <article className="service" key={service.id}>
+                <span className="service-icon">
+                  <i className={service.icon}></i>
+                </span>
+                <div className="service-info">
+                  <h4 className="service-title">{service.title}</h4>
+                  <p className="service-text">{service.text}</p>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Services;
